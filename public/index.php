71306c8fbaa5edab1a6347e1d919e28223ca05ed
alexokten <cemalokten@gmail.com>
2025-09-12 12:20:29 +0100

    WIP

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Spatie\Regex\Regex;
@@ -12,20 +11,21 @@ ray()->clearAll();
 class Router
 {
     private $routesList = [];
+    private $matchBracesAndSemiColons = '/\{\w+\}|:\w+/';
 
     public function dispatch(): void
     {
         $requestURL = $_SERVER['REQUEST_URI'];
 
         $req = ['params' => []];
-        $res = [];
-        $req['params'] = '123';
+        $res = ['send' => function () {
+            echo "Route accessed";
+        }];
 
         $route = $this->matchRoute($requestURL);
         $this->verifyMethod($route['method']);
-        // $req['params'] = $this->extractParams($requestURL);
-        //
-        $this->extractParams($route['slug']);
+        $extractedParamKeys = $this->extractParams($route['slug'], $requestURL);
+        $req = ['params' => $extractedParamKeys];
         $route['controller']($req, $res);
     }
 
@@ -85,6 +85,33 @@ class Router
             if ($route['slug'] === $requestSlug) {
                 return $route;
             }
+
+            $splitRouteSlug = explode('/', $route['slug']);
+            $splitRequestSlug = explode('/', $requestSlug);
+
+            if (count($splitRouteSlug) !== count($splitRequestSlug)) {
+                continue;
+            }
+
+            $isMatch = true;
+
+            for ($i = 0; $i < count($splitRouteSlug); $i++) {
+
+                $routeSegment = $splitRouteSlug[$i];
+                $requestSegment = $splitRequestSlug[$i];
+
+                if (Regex::match($this->matchBracesAndSemiColons, $routeSegment)->hasMatch()) {
+                    continue;
+                }
+
+                if ($routeSegment !== $requestSegment) {
+                    $isMatch = false;
+                    break;
+                }
+            }
+            if ($isMatch) {
+                return $route;
+            }
         }
         throw new Exception('Route not matched');
     }
@@ -98,21 +125,26 @@ class Router
         return true;
     }
 
-    private function extractParams(string $routeSlug): string
+    private function extractParams(string $routeSlugToMatch, string $requestSlugToMatch): array
     {
-        if (Regex::match('/\{\w+\}|:\w+/', $routeSlug)->hasMatch()) {
-            $params = [];
-            $matches = Regex::matchAll('/\{\w+\}|:\w+/', $routeSlug);
+        $extractedParams = [];
 
-            ray($matches);
-            foreach ($matches->results() as $match) {
-                ray($match);
+        $splitRouteSlug = explode('/', $routeSlugToMatch);
+        $splitRequestSlug = explode('/', $requestSlugToMatch);
+
+        for ($i = 0; $i < count($splitRouteSlug); $i++) {
+            $routeSegment = $splitRouteSlug[$i];
+            $requestSegment = $splitRequestSlug[$i];
+
+            if (Regex::match($this->matchBracesAndSemiColons, $routeSegment)->hasMatch()) {
+                $extractedParams = [...$extractedParams, $requestSegment];
             }
 
-
-            return Regex::match('/\{\w+\}|:\w+/', $routeSlug)->result();
+            if ($routeSegment !== $requestSegment) {
+                continue;
+            }
         }
-        return '';
+        return $extractedParams;
     }
 }
 
@@ -120,8 +152,8 @@ class Router
 
 $router = new Router();
 
-$router->get('/api/user/:id/:test', function ($req, $res) {
-    ray($req['params'], ': PARAMS');
+$router->get('/api/user/:id/:location', function ($req, $res) {
+    $res['send']();
 });
 
 $router->dispatch();
