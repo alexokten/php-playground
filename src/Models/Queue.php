0fcfb0537570ad4384f93cbcb5559e52e0069f3d
alexokten <cemalokten@gmail.com>
2025-10-03 12:21:02 +0100

    fix override issues

--- src/Models/Queue.php
+++ src/Models/Queue.php
@@ -12,11 +12,12 @@ class Queue extends Model
 {
     protected $table = 'queue';
 
-    const CREATED_AT = 'createdAt';
-    const UPDATED_AT = 'updatedAt';
+    const string CREATED_AT = 'createdAt';
+    const string UPDATED_AT = 'updatedAt';
 
     protected $guarded = ['id', 'createdAt', 'updatedAt'];
 
+    #[\Override]
     protected function casts()
     {
         return [
--- src/Models/Venues.php
+++ src/Models/Venues.php
@@ -10,11 +10,12 @@ class Venues extends Model
 {
     protected $table = 'venues';
 
-    const CREATED_AT = 'createdAt';
-    const UPDATED_AT = 'updatedAt';
+    const string CREATED_AT = 'createdAt';
+    const string UPDATED_AT = 'updatedAt';
 
     protected $guarded = ['id', 'createdAt', 'updatedAt'];
 
+    #[\Override]
     protected function casts()
     {
         return [
